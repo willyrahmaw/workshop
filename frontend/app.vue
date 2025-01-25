741189a6a476c539a26f8992fc93237add259aad
Willy Rahma Wijaya <sayawillyrw@gmail.com>
2025-01-25 19:50:15 +0700

    workshop

---
+++ frontend/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Hello from './components/Hello.vue';
+
+</script>
+
+<template>
+  <div>
+    <Hello></Hello>
+  </div>
+</template>
---
+++ frontend/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>Hello</h1>
+    <input v-model="value">
+    <button v-on:click="logThis(value)">Submit</button>
+    <br>
+    <span>{{ responseAPI }}</span>
+</template>
+
+<script lang="js">
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            value: 1,
+            responseAPI: ''
+        }
+    },
+    methods: {
+        async logThis(value) {
+            console.log(value);
+            try {
+                const response = await axios.get(`http://localhost:3001/ganjilgenap?value=${value}`);
+                console.log(response.data);
+                this.responseAPI = response.data
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+    }
+}
+
+</script>
